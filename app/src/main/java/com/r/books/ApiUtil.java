e587c0e6c1e4af29dfb305f14b37a7b4fb0aab84
w4wycliff <w4wycliff@gmail.com>
2019-06-20 14:38:05 +0300

    display results from book search

--- app/src/main/java/com/r/books/ApiUtil.java
+++ app/src/main/java/com/r/books/ApiUtil.java
@@ -1,13 +1,17 @@
 package com.r.books;
 
-import android.os.AsyncTask;
+import android.net.Uri;
 import android.util.Log;
-import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ApiUtil {
@@ -16,13 +20,19 @@ public class ApiUtil {
     }
 
     public static final String BASE_URL = "https://www.googleapis.com/books/v1/volumes";
+    public static final String QUERY_KEY_PARAMETER = "q";
+
 
     //create a URL
     public static URL buildUrl(String title) {
-        String fullUrl = BASE_URL + "?q=" + title;
+
         URL url = null;
+        Uri uri = Uri.parse(BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_KEY_PARAMETER, title)
+                .build();
+
         try{
-            url = new URL(fullUrl);
+            url = new URL(uri.toString());
         }
         catch (Exception e){
             e.printStackTrace();
@@ -68,6 +78,58 @@ public class ApiUtil {
             connection.disconnect();
         }
 
+    }
+
+
+    public static ArrayList<Book> getBooksFromJson(String json) {
+        final String ID = "id";
+        final String TITLE = "title";
+        final String SUBTITLE = "subtitle";
+        final String AUTHORS = "authors";
+        final String PUBLISHER = "publisher";
+        final String PUBLISHED_DATE = "publishedDate";
+        final String ITEMS = "items";
+        final String VOLUMEINFO = "volumeInfo";
+
+
+        ArrayList<Book> books = new ArrayList<Book>();
+
+        try {
+
+            JSONObject jsonBooks = new JSONObject(json);
+            JSONArray arrayBooks = jsonBooks.getJSONArray(ITEMS);
+            int numberOfBooks = arrayBooks.length();
+
+            for(int i=0; i<numberOfBooks; i++){
+                JSONObject bookJson = arrayBooks.getJSONObject(i);
+                JSONObject volumeInfoJson = bookJson.getJSONObject(VOLUMEINFO);
+                int authorNum = volumeInfoJson.getJSONArray(AUTHORS).length();
+
+                String[] authors = new String[authorNum];
+
+                for(int j=0; j<authorNum; j++){
+                    authors[j] = volumeInfoJson.getJSONArray(AUTHORS).get(j).toString();
+
+                }
+
+                Book book = new Book(
+                        bookJson.getString(ID),
+                        volumeInfoJson.getString(TITLE),
+                        (volumeInfoJson.isNull(SUBTITLE)?"":volumeInfoJson.getString(SUBTITLE)),
+                        authors,
+                        volumeInfoJson.getString(PUBLISHER),
+                        volumeInfoJson.getString(PUBLISHED_DATE));
+
+                books.add(book);
+            }
+
+
+        } catch(JSONException e){
+            e.printStackTrace();
+        }
+
+
+        return books;
     }
 
 
--- app/src/main/java/com/r/books/MainActivity.java
+++ app/src/main/java/com/r/books/MainActivity.java
@@ -4,26 +4,30 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import static com.r.books.ApiUtil.getJson;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar progressLoader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressLoader = findViewById(R.id.progressBar);
 
-
-        try{
+        try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
             new BooksQueryTask().execute(bookUrl);
-
-
         }
         catch(Exception e) {
             Log.d("Error", e.getMessage());
@@ -33,6 +37,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     public class BooksQueryTask extends AsyncTask<URL, Void ,String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressLoader.setVisibility(View.VISIBLE);
+        }
 
         @Override
         protected String doInBackground(URL... urls) {
@@ -50,7 +60,31 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             TextView bookResults = findViewById(R.id.books_results);
-            bookResults.setText(result);
+
+
+            if(result == null) {
+
+                bookResults.setVisibility(View.GONE);
+
+                Toast.makeText(MainActivity.this, "Error Loading data from Google API",
+                        Toast.LENGTH_LONG).show();
+                progressLoader.setVisibility(View.GONE);
+            }
+            else {
+
+                progressLoader.setVisibility(View.GONE);
+
+            }
+
+            ArrayList<Book> books = ApiUtil.getBooksFromJson(result);
+            String resultString = "";
+            for(Book book:books){
+                resultString = resultString + book.title + "\n" +
+                        book.publishedDate + "\n\n";
+                bookResults.setText(resultString);
+            }
+
+
         }
     }
 }
