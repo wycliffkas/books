e9cb1010a3852132f6b9a27c354843e34714c092
w4wycliff <w4wycliff@gmail.com>
2019-06-23 12:58:13 +0300

    add recycler view

--- app/src/main/java/com/r/books/MainActivity.java
+++ app/src/main/java/com/r/books/MainActivity.java
@@ -1,13 +1,20 @@
 package com.r.books;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.IOException;
 import java.net.URL;
@@ -15,15 +22,23 @@ import java.util.ArrayList;
 
 import static com.r.books.ApiUtil.getJson;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private ProgressBar progressLoader;
+    private RecyclerView booksRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         progressLoader = findViewById(R.id.progressBar);
+        booksRecyclerView = findViewById(R.id.mRecyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        booksRecyclerView.setLayoutManager(linearLayoutManager);
+
+
 
         try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
@@ -35,6 +50,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.book_search, menu);
+        final MenuItem searchItem = menu.findItem(R.id.bookSearch);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        try {
+            URL bookUrl = ApiUtil.buildUrl(query);
+            new BooksQueryTask().execute(bookUrl);
+
+        }
+        catch(Exception e){
+            Log.d("error", e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
 
     public class BooksQueryTask extends AsyncTask<URL, Void ,String> {
 
@@ -59,13 +102,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
-            TextView bookResults = findViewById(R.id.books_results);
 
 
             if(result == null) {
-
-                bookResults.setVisibility(View.GONE);
-
                 Toast.makeText(MainActivity.this, "Error Loading data from Google API",
                         Toast.LENGTH_LONG).show();
                 progressLoader.setVisibility(View.GONE);
@@ -73,16 +112,12 @@ public class MainActivity extends AppCompatActivity {
             else {
 
                 progressLoader.setVisibility(View.GONE);
+                ArrayList<Book> books = ApiUtil.getBooksFromJson(result);
+                BookAdapter adapter = new BookAdapter(books);
+                booksRecyclerView.setAdapter(adapter);
 
             }
 
-            ArrayList<Book> books = ApiUtil.getBooksFromJson(result);
-            String resultString = "";
-            for(Book book:books){
-                resultString = resultString + book.title + "\n" +
-                        book.publishedDate + "\n\n";
-                bookResults.setText(resultString);
-            }
 
 
         }
